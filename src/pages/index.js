b7bde4e5b0ad3739a041af1e175207e7e3c21117
armand-meskin <armandm101@gmail.com>
2024-02-14 14:58:10 -0500

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+//import Profile from './components/profile'
+//import List from './qcomps/list_keys_id'
+//import Recipes from './qcomps/recipes'
+//import Galley from './qcomps/state'
+import SF from './qcomps/stuckForm'
+
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <SF />
     </div>
   )
 }
