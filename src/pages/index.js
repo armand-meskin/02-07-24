f9792f11219b550abd698ebc175647ffe65de8c1
armand-meskin <armandm101@gmail.com>
2024-02-21 17:24:28 -0500

    Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+//import Profile from './components/profile'
+//import ArtRM from './qcomps/artistsRemoveArr'
+import Obj from './qcomps/arrObj'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Obj />
     </div>
   )
 }
